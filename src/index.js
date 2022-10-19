2fa9446174be92f57d3a970c87d1fca72d887ad7
Hungttph13998 <hungttph13998@fpt.edu.vn>
2022-10-19 17:57:41 +0700

    fix cors

--- src/index.js
+++ src/index.js
@@ -37,32 +37,7 @@ server.use((req, res, next) => {
   }
   next();
 });
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
 
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
-
-module.exports = allowCors(handler);
 server.use("/api", router);
 server.listen(8080, () => {
   console.log("JSON Server is running");
