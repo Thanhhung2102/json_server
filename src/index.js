9681e8286adeea42f2b579c6c12e4711a49e908e
Hungttph13998 <hungttph13998@fpt.edu.vn>
2022-10-19 18:23:44 +0700

    fix cors

--- src/index.js
+++ src/index.js
@@ -31,12 +31,6 @@ server.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
-  if (req.method === "POST") {
-    req.body.createAt = Date.now();
-    req.body.updateAt = Date.now();
-  } else if (req.method === "PATCH") {
-    req.body.updateAt = Date.now();
-  }
   next();
 });
 
